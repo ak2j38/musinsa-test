93b4692e34058d62088d97c2fb7a7cb88e89a38e
WooJin Park(Ader) <ak2j38@gmail.com>
2022-07-17 04:50:48 +0900

    ✨ 전역 예외처리 클래스 구현

---
+++ src/main/java/com/parkwoojin/musinsa/exception/ExistSubCategoryException.java
@@ -0,0 +1,8 @@
+package com.parkwoojin.musinsa.exception;
+
+public class ExistSubCategoryException extends RuntimeException {
+
+    public ExistSubCategoryException(String message) {
+        super(message);
+    }
+}
